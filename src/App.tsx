70841ef2b74e0ec9743d47619c2b175576e653b8
alexander matiugin <alexander.matiugin@pandadoc.com>
2021-09-14 13:07:10 +0300

    add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import { NavLink } from 'react-router-dom';
+import { SignIn } from './SignIn';
 
 const App = (): JSX.Element => {
   return (
     <>
       <Switch>
         <Route exact path="/" render={() => (<div>Home</div>)} />
+        <Route exact path="/signin" render={SignIn} />
         <Route render={() => (<div>Not found.</div>)} />
       </Switch>
       <NavLink to="/">
